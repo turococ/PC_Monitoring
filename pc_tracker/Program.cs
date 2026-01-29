b77b0ba20a18c556718af3eda5412eae871806a9
turococ <84099957+turococ@users.noreply.github.com>
2026-01-29 14:05:07 +0300

    push

---
+++ pc_tracker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using Spectre.Console;
+
+var CPU = "intel";
+var GPU = "nvidia";
+var RAM = "16gb";
+
+
+Console.Clear();
+AnsiConsole.WriteLine();
+var PC_Spec = new Panel(
+	new Rows(
+		new Markup($"[bold] Cpu: {CPU}[/]"),
+		new Markup($"[bold] Gpu: {GPU}[/]"),
+		new Markup($"[bold] Ram: {RAM}[/]")
+		)
+	);
+PC_Spec.Header = new PanelHeader("[bold] PC Specifications [/]");
+PC_Spec.Border = BoxBorder.Rounded;
+
+
+AnsiConsole.Write(PC_Spec);
+AnsiConsole.WriteLine();
