93caf2f7e4618d4289922a922c16dfe8a625c4a1
turococ <84099957+turococ@users.noreply.github.com>
2026-02-07 17:47:42 +0300

    Large Update
    вся система была переведена из консоли в приложение, немного сделан интерфейс, работы продолжаются
    the entire system has been transferred from the console to the application, the interface has been slightly modified, and work is ongoing

---
+++ PcMonitoring/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using HardwareMonitor.Hardware;
+using PcMonitoring.UI;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HardwareMonitor;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var reader = new HardwareReader();
+        var specs = reader.ReadPcSpec();
+
+        DataContext = new PcSpecsViewModel(specs);
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+            DragMove();
+    }
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+}
---
+++ PcMonitoring/UI/PcSpecsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HardwareMonitor.Hardware;
+
+namespace PcMonitoring.UI;
+
+public class PcSpecsViewModel
+{
+    public string MotherBoard { get; }
+    public string Cpu { get; }
+    public string Gpu { get; }
+    public string Ram { get; }
+    public string SSD { get; }
+    public string HDD { get; }
+
+    public PcSpecsViewModel(PcSpecs specs)
+    {
+        MotherBoard = $"MotherBoard: {specs.MotherBoard}";
+        Cpu = $"CPU: {specs.Cpu}";
+        Gpu = $"GPU: {specs.Gpu}";
+        Ram = $"RAM: {specs.Ram}";
+        SSD = $"SSD: {specs.SSD}";
+        HDD = $"HDD: {specs.HDD}";
+    }
+}
