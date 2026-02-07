cb75a7297e910bc629a983d9c3f29d1a8d45ba91
turococ <84099957+turococ@users.noreply.github.com>
2026-02-07 18:00:20 +0300

    hot fix

--- PcMonitoring/obj/Debug/net10.0-windows/PcMonitoring.AssemblyInfo.cs
+++ PcMonitoring/obj/Debug/net10.0-windows/PcMonitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65745ffc267d0183a4f30c58213cb24040640bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39346bda550e1253e4297d502f5bee5abf2c319a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
