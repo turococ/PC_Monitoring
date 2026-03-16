4da81e8b3ded449de1f3658b34af561b12adefb9
turococ <84099957+turococ@users.noreply.github.com>
2026-03-16 11:50:36 +0300

    update

--- PcMonitoring/obj/Debug/net10.0-windows/PcMonitoring.AssemblyInfo.cs
+++ PcMonitoring/obj/Debug/net10.0-windows/PcMonitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac8cff0eb8f78ad85da3ad5195a1b69a80172406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e242e967ae653f258aad18c87924eedd1ba6622")]
 [assembly: System.Reflection.AssemblyProductAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PcMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
